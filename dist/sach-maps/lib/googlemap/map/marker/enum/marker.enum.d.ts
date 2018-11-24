49cc39880d20814d386094bf900d997b40f27267
chsami <sami.chkhachkhi@gmail.com>
2018-11-24 19:44:47 +0100

    revert dist folder

--- dist/sach-maps/lib/googlemap/map/marker/enum/marker.enum.d.ts
+++
@@ -1,5 +0,0 @@
-export declare enum MarkerStateEnum {
-    CREATION = 0,
-    EDIT = 1,
-    IDLE = 2
-}
--- dist/sach-maps/public_api.d.ts
+++
@@ -1,6 +0,0 @@
-export { MapService } from './lib/googlemap/map/map.service';
-export { MapModule } from './lib/googlemap/map/map.module';
-export { MarkerService } from './lib/googlemap/map/marker/marker.service';
-export { MarkerModule } from './lib/googlemap/map/marker/marker.module';
-export { Marker } from './lib/googlemap/map/marker/models/marker.model';
-export { MarkerStateEnum } from './lib/googlemap/map/marker/enum/marker.enum';
